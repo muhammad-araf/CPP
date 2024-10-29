8b9572ab4fdd26a2f097d97bd11b261c2730750b
lentoaraf0 <arafmuhammad2008@gmail.com>
2024-10-30 00:11:27 +0500

    Simple Calculator

---
+++ calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if (operation == '+')
+    {
+        cout << "Result: " << num1 + num2 << endl;
+    }
+    else if (operation == '-')
+    {
+        cout << "Result: " << num1 - num2 << endl;
+    }
+
+    else if (operation == '*')
+        cout << "Result: " << num1 * num2 << endl;
+    else if (operation == '/')
+    {
+        if (num2 != 0)
+            cout << "Result: " << num1 / num2 << endl;
+        else
+            cout << "Error: Division by zero!" << endl;
+    }
+    else
+    {
+        cout << "Error: Invalid operation!" << endl;
+    }
+
+    return 0;
+}
