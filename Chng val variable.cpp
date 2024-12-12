7bebf404b5e41a44d1f09017badab77543f4c5ae
Muhammad ARAF <lentoaraf@gmail.com>
2024-12-12 08:59:08 +0500

    Update Chng val variable.cpp

--- Chng val variable.cpp
+++ Chng val variable.cpp
@@ -3,15 +3,13 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "Enter a Number that store in a variable ";
-    cin >> a;
-    cout << "Enter a Number that store in b variable ";
-    cin >> b;
+    a = 5;
+    b = 10;
     // Swap values
     a = a + b; // a = 15
     b = a - b; // b = 5
     a = a - b; // a = 10
 
-    cout << "a = " << a << ", b = " << b << endl; // Output:  a= 10, b = 5
+    cout << "a = " << a << ", b = " << b << endl; // Output:  a = 10, b = 5
     return 0;
 }
