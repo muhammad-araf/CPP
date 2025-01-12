6087dd103ffd6d787f77d240836df67e193da69e
lentoaraf0 <lentoaraf@gmail.com>
2025-01-12 07:52:05 +0500

    Q-06

---
+++ Dr.sheraz 30 days Assignment/06_Loop_Control_With_Conditions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 2; i <= 25; i++)
+    {
+        if (i == 13 || i == 16 || i == 19)
+        {
+            continue;
+        }
+        if (i == 23)
+        {
+            break;
+        }
+        cout << i << endl;
+    }
+
+        return 0;
+}
