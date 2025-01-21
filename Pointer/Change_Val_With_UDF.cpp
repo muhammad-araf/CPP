50f7f2d0a410fd0858e60ab72c50eceb24185269
m-araf <lentoaraf@gmail.com>
2025-01-21 20:59:24 +0500

    Interchange the value of 2 variable with pointer and udf

---
+++ Pointer/Change_Val_With_UDF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void swapNumbers(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1 = 5, num2 = 10;
+
+    cout << "Before swapping: num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    swapNumbers(&num1, &num2);
+
+    cout << "After swapping: num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    return 0;
+}
