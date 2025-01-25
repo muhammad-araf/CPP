325993b178c34a5f137bc04a65acb0797ac14be6
m-araf <lentoaraf@gmail.com>
2025-01-25 12:51:16 +0500

    correct my code

--- Dr.sheraz 30 days Assignment/08_Merged_Array_And_Descending_Order.cpp
+++ Dr.sheraz 30 days Assignment/08_Merged_Array_And_Descending_Order.cpp
@@ -1,30 +1,27 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
 int main()
 {
-    //************ Take input of Arr 1 *************
+    //************ Arr 1 *************
     int len1;
-    cout << "Input the number of elements to be stored in the first array : ";
-    cin >> len1;
-    cout << "Input " << len1 << " elements in the array : " << endl;
+    printf("Input the number of elements to be stored in the first array : ");
+    scanf("%d", &len1);
+    printf("Input %d elements in the array : \n", len1);
     int arr[100];
     for (int i = 0; i < len1; i++)
     {
-        cout << "element - " << i << " ";
-        cin >> arr[i];
+        printf("element - %d ", i);
+        scanf("%d", &arr[i]);
     }
-
-    //************ Take input of Arr 2 (same as Arr 1 )*************
+    //************ Arr 2 *************
     int len2;
-    cout << "Input the number of elements to be stored in the first array : ";
-    cin >> len2;
-    cout << "Input " << len2 << " elements in the array : " << endl;
+    printf("Input the number of elements to be stored in the first array : ");
+    scanf("%d", &len2);
+    printf("Input %d elements in the array : ", len2);
     int arr2[100];
     for (int i = 0; i < len2; i++)
     {
-        cout << "element - " << i << " ";
-        cin >> arr2[i];
+        printf("element - %d ", i);
+        scanf("%d", &arr2[i]);
     }
     //************ Arr 3 for merging *************
     int arr3[100];
@@ -42,7 +39,7 @@ int main()
     {
         for (int i = 0; i < j; i++)
         {
-            if (arr3[i] > arr3[i + 1])
+            if (arr3[i] < arr3[i + 1])
             {
                 int temp = arr3[i];
                 arr3[i] = arr3[i + 1];
@@ -50,11 +47,24 @@ int main()
             }
         }
     }
-    cout << "The merged array in Descending order is : " << endl;
+    printf("The merged array in Descending order is : \n");
     for (int i = 0; i < (len1 + len2); i++)
     {
-        cout << arr3[i];
+        printf("%d", arr3[i]);
     }
-
-    return 0;
+    //************ Arr 3 Count Repeating Element in Arr3  *************
+    int count = 0;
+    for (int i = (len1 + len2) - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr3[i] == arr3[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("\nTotal number of duplicate elements found in the array is : ");
+    printf("%d", count);
 }
