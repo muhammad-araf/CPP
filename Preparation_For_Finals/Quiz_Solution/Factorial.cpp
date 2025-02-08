46de5cea7f698fee68fc1127633031d60860c41d
m-araf <lentoaraf@gmail.com>
2025-02-09 04:43:17 +0500

    Calculate Factorial of number User input

---
+++ Preparation_For_Finals/Quiz_Solution/Factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    int fact = 1;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int i = 2; i <= num; i++)
+    {
+        fact *= i;
+    }
+    cout << "Factorial of " << num << " is " << fact << endl;
+
+    return 0;
+}
