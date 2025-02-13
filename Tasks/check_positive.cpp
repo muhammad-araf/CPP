c33becac66e1f46b26503308788b06302021fb89
Muhammad ARAF <lentoaraf@gmail.com>
2025-02-13 03:03:35 +0000

    Check number is positive or not

---
+++ Tasks/check_positive.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num
+    cout<<"Enter a number to check it is Positive or Negative : ";
+    cin>>num;
+    if(num>0){
+
+        cout<<"Number is positive";
+    }else{
+        cout<<"Number is Negative";
+    }
+    return 0;
+}
